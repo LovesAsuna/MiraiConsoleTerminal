dd11eab7a4435b3ff4b79b44e951f208f1e6b2be
LovesAsuna <qq625924077@gmail.com>
2021-12-12 10:21:27 +0800

    :bug: fix(bug): log4j2 bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,25 +13,25 @@ repositories {
 
 // Log
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.15.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.15.0")
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.5")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.15.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
 }
 
 // Mirai
 dependencies {
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
-    compileOnly("net.mamoe:mirai-core:2.7.0")
-    compileOnly("net.mamoe:mirai-console:2.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
+    compileOnly("net.mamoe:mirai-core:2.8.2")
+    compileOnly("net.mamoe:mirai-console:2.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 // Kotlin
 dependencies {
-    implementation("org.fusesource.jansi:jansi:2.3.4")
+    implementation("org.fusesource.jansi:jansi:2.4.0")
     implementation("org.jline:jline:3.20.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
 }
