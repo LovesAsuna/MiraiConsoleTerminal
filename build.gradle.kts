f324212bdd34002d71e611ad4a2c814cb6683687
LovesAsuna <qq625924077@gmail.com>
2022-01-05 13:27:15 +0800

    :arrow_up: build(gradle): upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,19 +13,19 @@ repositories {
 
 // Log
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.16.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.0")
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.16.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
 }
 
 // Mirai
 dependencies {
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
-    compileOnly("net.mamoe:mirai-core:2.8.3")
-    compileOnly("net.mamoe:mirai-console:2.8.3")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
+    compileOnly("net.mamoe:mirai-core:2.9.1")
+    compileOnly("net.mamoe:mirai-console:2.9.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
